614bce7389a4fade3a463e378de86117d1bb1f1c
MassivDash <lukasz.celitan@gmail.com>
2018-04-15 01:55:38 +0200

    more cms changes

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,11 +1,11 @@
 import CMS from 'netlify-cms'
 import OfundacjiPagePreview from './preview-templates/OfundacjiPagePreview'
-import ProjektgatPostPreview from './preview-templates/ProjektPostPreview'
+import ProjektPostPreview from './preview-templates/ProjektPostPreview'
 import CelePagePreview from './preview-templates/CelePagePreview'
 import KontaktPagePreview from './preview-templates/KontaktPagePreview'
 
 CMS.registerPreviewStyle('/styles.css')
 CMS.registerPreviewTemplate('ofundacji', OfundacjiPagePreview)
 CMS.registerPreviewTemplate('projket', ProjektPostPreview)
-CMS.registerPreviewTemplate('cele', ProjektPostPreview)
-CMS.registerPreviewTemplate('kontakt', ProjektPostPreview)
+CMS.registerPreviewTemplate('cele', CelePagePreview)
+CMS.registerPreviewTemplate('kontakt', KontaktPostPreview)
