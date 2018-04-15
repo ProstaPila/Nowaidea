73f57e781ddf48cfaa02977e37587fb244c50c25
MassivDash <lukasz.celitan@gmail.com>
2018-04-16 00:13:19 +0200

    added more fields to the sites

--- src/cms/preview-templates/ProjektPostPreview.js
+++ src/cms/preview-templates/ProjektPostPreview.js
@@ -8,6 +8,7 @@ const ProjektPostPreview = ({ entry, widgetFor }) => (
     description={entry.getIn(['data', 'description'])}
     tags={entry.getIn(['data', 'tags'])}
     title={entry.getIn(['data', 'title'])}
+    thumbnail={entry.getIn(['data', 'thumbnail' ])}
   />
 )
 
