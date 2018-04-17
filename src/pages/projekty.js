fd9c39a30877f734c6f2ab4fd2bca99bd931ffb6
MassivDash <lukasz.celitan@gmail.com>
2018-04-17 13:45:11 +0200

    projekty page

--- src/pages/projekty.js
+++ src/pages/projekty.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
+import thumbnail from '../img/background.jpg'
 
 
 export default class ProjektyPage extends React.Component {
