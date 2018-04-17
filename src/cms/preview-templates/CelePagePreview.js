73d89abfff38d4c2edfe309966cd6051ae3be276
MassivDash <lukasz.celitan@gmail.com>
2018-04-17 03:00:08 +0200

    added styles for the preview

--- src/cms/preview-templates/CelePagePreview.js
+++ src/cms/preview-templates/CelePagePreview.js
@@ -1,28 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { CelePageTemplate } from '../../templates/cele-page'
+import '../../layouts/all.sass'
 
-const CelePagePreview = ({ entry, widgetFor }) => (
-  <CelePageTemplate
-    title={entry.getIn(['data', 'title'])}
-    content={widgetFor('body')}
-    slug={entry.getIn(['fields', 'slug'])}
-    thumbnail={entry.getIn(['data', 'thumbnail' ])}
-    postPath={entry.getIn(['data', 'slug'])}
-    description={entry.getIn(['data', 'description'])}
-    helmet={entry.getIn(['data', 'title'])}
-      postNode={{
-      frontmatter: {
-        description: entry.getIn(['data', 'description']),
-        title: entry.getIn(['data', 'title']),
-        thumbnail: entry.getIn(['data', 'thumbnail' ])   
-
-      }
-      
-
-      }}
+const CelePagePreview = ({ entry, widgetFor, getAsset }) => {
+  const data = entry.get('data').toJS();
+  const { title, content, slug, thumbnail, postPath, decription, helmet, postNode } = data;
+  return <CelePageTemplate
+    title={title}
+    content={content}
+    slug={slug}
+    thumbnail={thumbnail}
+    postPath={postPatg}
+    description={description}
+    helmet={helmet}
+    postNode={postNode}
   />
-)
+}
 
 CelePagePreview.propTypes = {
   entry: PropTypes.shape({
