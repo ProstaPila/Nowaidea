48809159eb99824c5707a827317e0c6e77c084b2
ProstaPila <161878749+ProstaPila@users.noreply.github.com>
2024-03-09 20:20:51 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -71,7 +71,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-favicon`,
       options: {
-        logo: "src/img/logo.png",
+        logo: "/src/img/logo.png",
         injectHTML: true,
         icons: {
           android: true,
